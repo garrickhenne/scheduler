d227a68fed85ab54c33b1069959e9d84792f99d2
Garrick Henne <garrickhenne@outlook.com>
2023-11-08 13:17:16 -0800

    lint Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -32,31 +32,32 @@ export default function Form(props) {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+        <form autoComplete="off" onSubmit={ event => event.preventDefault() }>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={name}
-            onChange={event => {
+            value={ name }
+            onChange={ event => {
               setName(event.target.value);
-            }}
+            } }
+            data-testid='student-name-input'
           />
         </form>
-        <section className="appointment__validation">{error}</section>
+        <section className="appointment__validation">{ error }</section>
         <InterviewerList
-          interviewers={props.interviewers}
-          value={interviewer}
-          onChange={setInterviewer}
+          interviewers={ props.interviewers }
+          value={ interviewer }
+          onChange={ setInterviewer }
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={cancel}>
+          <Button danger onClick={ cancel }>
             Cancel
           </Button>
-          <Button confirm onClick={validate}>
+          <Button confirm onClick={ validate }>
             Save
           </Button>
         </section>
