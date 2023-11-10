fca749eefa623505664a5a01b34a76441a6fb403
Garrick Henne <garrickhenne@outlook.com>
2023-11-09 21:33:22 -0800

    fail test

--- src/reducers/application.js
+++ src/reducers/application.js
@@ -1,4 +1,4 @@
-export const SET_DAY = "SET_DAY";
+// export const SET_DAY = "SET_DAY";
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 export const SET_INTERVIEW = "SET_INTERVIEW";
 
@@ -34,9 +34,9 @@ export default function reducer(state, action) {
       const days = state.days.map(day => {
         return day.appointments.includes(action.id)
           ? {
-              ...day,
-              spots: getSpotsForDay(day)
-            }
+            ...day,
+            spots: getSpotsForDay(day)
+          }
           : day;
       });
 
